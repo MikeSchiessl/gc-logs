8f0940a37fe6924d5fd6888edeb0c1b438e5c394
Mike Schiessl <mschiess@akamai.com>
2022-08-22 13:59:58 +0200

    fixed time issues, added line breaker

--- bin/config/default_config.py
+++ bin/config/default_config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 # Delay from $now to ensure 100% of logs are received on the backend (time in seconds)
-log_delay = 600
+log_delay = 1200
 
 # Loop time in seconds
-loop_time = 600
+loop_time = 30
 
 
